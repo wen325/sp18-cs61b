95de187eb2d90280c6b52ea52618572ac57452ab
wen325 <xiangwen325@gmail.com>
2021-12-06 21:48:49 +0800

    first commit of proj1a

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,87 @@
+public class ArrayDeque<Item> {
+
+	private Item[] items;
+	private int size;
+	private int sentinel_front;
+	private int sentinel_back;
+	private int start = 4;
+
+	/** Creates an empty list. */
+	public ArrayDeque() {
+		items = (Item[]) new Object[8];
+		sentinel_front = start;
+		sentinel_back = start;
+		size = 0;
+	}
+
+	/** Creates a list with X. */
+	public ArrayDeque(Item x){
+		items = (Item[]) new Object[8];
+		items[start] = x;
+		sentinel_front = current -1;
+		sentinel_back = current + 1;
+		size = 1;
+	}
+
+	/** Returns true if deque is empty, false otherwise. */
+	public boolean isEmpty(){
+		if (items == null){
+			return true;
+		}
+		return false;
+	}
+
+	/** Prints the items in the deque from first to last, separated by a space. */
+	public void printDeque(){
+		for(int i = sentinel_front + 1; i< sentinel_back; i++){
+			System.out.print(get(i) + " ");
+		}
+	}
+
+	/** Inserts X into the front of the list. */
+	public void addFirst(Item x){
+		items[sentinel_front] = x;
+		sentinel_front -= 1;
+		size += 1;
+	}
+
+	/** Removes and returns the item at the front of the deque. If no such item exists, returns null. */
+	public Item removeFirst(){
+		Item x = get(0);
+		sentinel_front += 1;
+		items[sentinel_front - 1] = null;
+		return x;
+	}
+
+	/** Inserts X into the back of the list. */
+	public void addLast(Item x) {
+		items[sentinel_back] = x;
+		sentinel_back -= 1;
+		size += 1;
+	}
+
+	/** Returns the item from the back of the list. */
+	public Item getLast() {
+		return items[sentinel_back - 1];
+	}
+
+	/** Deletes item from back of the list and
+	 * returns deleted item. */
+	public Item removeLast() {
+		Item x = items[sentinel_back - 1];
+		items[sentinel_back - 1] = null;
+		size = size - 1;
+		return x;
+	}
+
+	/** Gets the ith item in the list (0 is the front). */
+	public Item get(int i) {
+		return items[i + sentinel_front + 1];
+	}
+
+	/** Returns the number of items in the list. */
+	public int size() {
+		return size;
+	}
+
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,99 @@
+public class LinkedListDeque<BleepBlorp> {
+    private Deque sentinel_front;
+    private Deque sentinel_back;
+    private int size;
+
+    public class Deque {
+        public BleepBlorp item;
+        public Deque next;
+
+        public Deque(BleepBlorp i, Deque n) {
+            item = i;
+            next = n;
+        }
+    }
+
+    /** Initializ the null Deque */
+    public LinkedListDeque() {
+        sentinel_front = new Deque(null, null);
+        sentinel_back = new Deque(null, null);
+        size = 0;
+    }
+
+    /** Initialize the Deque with x */
+    public LinkedListDeque(BleepBlorp x) {
+        sentinel_front = new Deque(null, null);
+        sentinel_front.next = new Deque(x, null);
+        sentinel_back.next = new Deque(x, null);
+        size = 1;
+    }
+
+    /**
+     * gets the item at the given index, where 0 is the front, 1 is the next item
+     */
+    public BleepBlorp get(int x) {
+        Deque p = sentinel_front;
+        while (x > 0) {
+            p = p.next;
+            if(p == null){
+                return null;
+            }
+            x -= 1;
+        }
+        return p.item;
+    }
+
+    /** add x to the First */
+    public void addFirst(BleepBlorp x) {
+        sentinel_front.next = new Deque(x, sentinel_front.next);
+        size += 1;
+    }
+
+    /** return first item of Deque */
+    public BleepBlorp getFirst() {
+        return sentinel_front.next.item;
+    }
+
+    /** remove and return the item at the front; if no item exists, return null */
+    public BleepBlorp removeFirst() {
+        BleepBlorp k = getFirst();
+        sentinel_front.next = sentinel_front.next.next;
+        size -= 1;
+        return k;
+    }
+
+    /** add x to the Last */
+    public void addLast(BleepBlorp x) {
+        sentinel_back.next = new Deque(x, null);
+        sentinel_back = sentinel_back.next;
+        size += 1;
+    }
+
+    /** return last item of Deque */
+    public BleepBlorp getLast() {
+        return sentinel_back.next.item;
+    }
+
+    /** remove and return the item at the back; if no item exists, return null */
+
+    /** return Deque length */
+    public int size(){
+        return size;
+    }
+
+    /** Returns true if deque is empty, false otherwise. */
+    public boolean isEmpty() {
+//        if(sentinel_front.next == null){
+//            return false;
+//        }
+        return (sentinel_front.next == null);
+//        return true;
+    }
+
+    /** Prints the items in the deque from first to last, separated by space. */
+    public void printDeque(){
+        for(int i = 0; i < size; i++){
+            System.out.print(get(i)+" ");
+        }
+    }
+}
