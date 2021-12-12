2c5e8a0233ae7f533e48523cc898cbe8bb065e0a
wen325 <xiangwen325@gmail.com>
2021-12-12 10:05:01 +0800

    completed

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -18,23 +18,27 @@ public class TestArrayDequeGold {
 				sad1.addLast(numberBetweenZeroAndNine);
 				sad2.addLast(numberBetweenZeroAndNine);
 				message = message + "addLast(" + numberBetweenZeroAndNine + ")\n";
+				assertEquals(message, sad1.size(), sad2.size());
 			} else if (numberBetweenZeroAndOne < 0.5) {
 				sad1.addFirst(numberBetweenZeroAndNine);
 				sad2.addFirst(numberBetweenZeroAndNine);
 				message = message + "addFirst(" + numberBetweenZeroAndNine + ")\n";
+				assertEquals(message, sad1.size(), sad2.size());
 			} else if (numberBetweenZeroAndOne < 0.75){
-				if(sad2.size() > 1) {
+				if(sad2.size() > 0) {
 					Integer a = sad1.removeFirst();
 					Integer b = sad2.removeFirst();
-					message = message + "removeFirst(): " + a + "\n";
+					message = message + "removeFirst()\n";
 					assertEquals(message, a, b);
+					assertEquals(message, sad1.size(), sad2.size());
 				}
 			} else{
-				if (sad2.size() > 1) {
+				if (sad2.size() > 0) {
 					Integer a = sad1.removeLast();
 					Integer b = sad2.removeLast();
-					message = message + "removeLast(): " + b + "\n";
+					message = message + "removeLast()\n";
 					assertEquals(message, a, b);
+					assertEquals(message, sad1.size(), sad2.size());
 				}
 			}
 		}
