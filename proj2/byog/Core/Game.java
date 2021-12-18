168ebdcb24bdc56454a42ff67466a2d56cad3c2a
wen325 <xiangwen325@gmail.com>
2021-12-18 22:02:09 +0800

    phase 1

--- proj2/byog/Core/Game.java
+++ proj2/byog/Core/Game.java
@@ -33,15 +33,9 @@ public class Game {
         // TODO: Fill out this method to run the game using the input passed in,
         // and return a 2D tile representation of the world that would have been
         // drawn if the same inputs had been given to playWithKeyboard().
-        int k = 1;
-        StringBuilder inputSEED = new StringBuilder();
-        char[] inputChar = input.toCharArray();
-        while (inputChar[k] != 'S') {
-            inputSEED.append(inputChar[k]);
-            k++;
-        }
 
-        long SEED = Long.parseLong(inputSEED.toString());
+
+        long SEED = Long.parseLong(input.replaceAll("[^0-9]",""));
 
         Random random = new Random(SEED);
 
