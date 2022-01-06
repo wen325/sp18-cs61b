840a41aa2549bef1ff4bdb8a681f91527c4dde00
wen325 <xiangwen325@gmail.com>
2022-01-06 11:20:51 +0800

    completed hw4

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -122,6 +122,10 @@ public class Board implements WorldState{
 		return true;
 	}
 
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
 
 	/** Returns the string representation of the board.
       * Uncomment this method. */
